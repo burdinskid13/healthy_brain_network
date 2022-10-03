ab37191e8c4463aa5d0b2cec9cd3fc5e16729a08
Maedbh King <mking86@uwo.ca>
2022-10-03 09:38:49 -0400

    fixed bug in run phenotype

--- hbn/scripts/run_phenotype_workflow.py
+++ hbn/scripts/run_phenotype_workflow.py
@@ -211,7 +211,7 @@ def run_model_pipeline_firstlevel(
         out_dir = glob.glob(os.path.join(os.getcwd(), '*out-localspec*'))
         shutil.copy(spec_fpath, out_dir[0])
         shutil.move(out_dir[0], Defaults.MODEL_DIR)
-        shutil.rmtree("messages")
+        # shutil.rmtree("messages")
 
 
 def run_model_pipeline_secondlevel():
