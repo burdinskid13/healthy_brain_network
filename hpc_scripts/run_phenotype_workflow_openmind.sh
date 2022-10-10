be02247f58d4ec02673f3121b09b5b7e4e0a0ad4
Maedbh King <mking86@uwo.ca>
2022-10-10 17:15:10 -0400

    add openmind bash script

---
+++ hpc_scripts/run_phenotype_workflow_openmind.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=workflow_phenotype_hbn
+#
+# Partition:
+#SBATCH --partition=gablab
+#
+# Nodes:
+# SBATCH -N 1 # one node
+#
+# Cores:
+#SBATCH -n 1 # one CPU (hyperthreaded) cores
+#
+# Wall clock limit:
+#SBATCH --time=03:00:00 # 3 hours
+# 
+# Email Updates:
+# SBATCH --mail-user=maedbh@mit.edu
+# SBATCH --mail-type=BEGIN,END,FAIL,REQUEUE,STAGE_OUT
+
+## Command(s) to run:
+module load openmind/anaconda/3-2022.05 # load python module
+source ~/.bash_profile # set paths
+source $(pipenv --venv)/bin/activate # activate virtual environment
+
+# scripts are stored here:
+cd /om2/user/maedbh/healthy_brain_network/hbn/scripts
+
+# run workflow
+python3 run_phenotype_workflow.py --feature-specs --model-specs --run-models-first --run-models-second
--- hpc_scripts/run_phenotype_workflow_savio.sh
+++ hpc_scripts/run_phenotype_workflow_savio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Job name:
-#SBATCH --job-name=workflow_phenotype_hbn_second_lvl
+#SBATCH --job-name=workflow_phenotype_hbn
 #
 # Account:
 #SBATCH --account=fc_cerebellum
@@ -12,7 +12,7 @@
 #SBATCH --qos=savio_normal
 #
 # Wall clock limit:
-#SBATCH --time=01:00:00
+#SBATCH --time=50:00:00
 
 ## Command(s) to run:
 module load python/3.9.12
@@ -22,4 +22,4 @@ source $(pipenv --venv)/bin/activate
 cd /global/scratch/users/maedbhking/projects/healthy_brain_network/hbn/scripts
 
 # run workflow
-python3 run_phenotype_workflow.py --no-feature-specs --no-model-specs --no-run-models-first --run-models-second
+python3 run_phenotype_workflow.py --feature-specs --model-specs --run-models-first --run-models-second
