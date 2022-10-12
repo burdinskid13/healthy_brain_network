ca383b3b95c25391abc7398c3e37601fb8df12ab
Maedbh King <mking86@uwo.ca>
2022-10-12 10:53:42 -0400

    updated bash script

--- hpc_scripts/run_phenotype_workflow_savio.sh
+++ hpc_scripts/run_phenotype_workflow_savio.sh
@@ -12,7 +12,7 @@
 #SBATCH --qos=savio_normal
 #
 # Wall clock limit:
-#SBATCH --time=50:00:00
+#SBATCH --time=01:00:00
 
 ## Command(s) to run:
 module load python/3.9.12
@@ -22,4 +22,4 @@ source $(pipenv --venv)/bin/activate
 cd /global/scratch/users/maedbhking/projects/healthy_brain_network/hbn/scripts
 
 # run workflow
-python3 run_phenotype_workflow.py --feature-specs --model-specs --run-models-first --run-models-second
+python3 run_phenotype_workflow.py --no-feature-specs --no-model-specs --no-run-models-first --run-models-second --no-run-locally
