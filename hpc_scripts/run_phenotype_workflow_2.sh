7d44ab1783118b4bdbe78b3001ce7c3f9d4cef13
Maedbh King <mking86@uwo.ca>
2022-12-12 18:26:54 -0500

    add sex as target variable

---
+++ hpc_scripts/run_phenotype_workflow_2.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=workflow_phenotype_hbn_2
+#
+# Partition:
+#SBATCH --partition=gablab
+#
+# Nodes:
+#SBATCH -N 1 # one node
+#
+# Tasks:
+#SBATCH -c 16 # 16 hyperthreaded cores 
+#
+# Memory:
+#SBATCH --mem=15G
+#
+# Wall clock limit:
+#SBATCH --time=10:00:00 # 
+# 
+# Email Updates:
+#SBATCH --mail-user=maedbh@mit.edu
+#SBATCH --mail-type=BEGIN,END,FAIL,REQUEUE,STAGE_OUT
+
+## Command(s) to run:
+module load openmind/anaconda/3-2022.05 # load python module
+source ~/.bash_profile # set paths
+source $(pipenv --venv)/bin/activate # activate virtual environment
+
+username=maedbh
+
+cd /om2/user/${username}/healthy_brain_network/hbn/scripts
+
+# preprocess
+#python3 preprocess_phenotype.py
+
+# # make features
+python3 make_phenotype_specs.py
+
+# # make model specs
+python3 make_phenotype_models.py --pydraml_spec=pydraml_spec2.json --target=target_Sex_binarize-spec.json --participants="['train_participants-ADHD.csv', 'train_participants-No_Diagnosis_Given.csv']"
+
+# run workflow
+python3 run_phenotype_models.py --first-level --second-level --cachedir=/om2/user/${username}/bin/.cache/pydra-ml/cache-wf/
+
+# delete pydra-ml cache from openmind (takes up to omuch space)
+#python3 delete_cache.py --cachedir=/om2/user/${username}/bin/.cache/pydra-ml/cache-wf/
