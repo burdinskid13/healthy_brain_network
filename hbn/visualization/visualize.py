d05af953b4a3a61d0521fa83fe006afd8a0098bd
Maedbh King <mking86@uwo.ca>
2023-02-01 18:09:46 -0500

    update notebooks

--- hbn/visualization/visualize.py
+++ hbn/visualization/visualize.py
@@ -127,7 +127,7 @@ def predictive_modeling(df, x='features', y='roc_auc_score'):
     fig.update_traces(meanline_visible=True, box_visible=False)
     fig.update_layout(violingap=0, violinmode='overlay')
     fig.update_xaxes(showticklabels=False)
-    fig.update_yaxes(title_text=y)
+    fig.update_yaxes(title_text=y, range=[0.4, 1])
     fig.show()
 
 
@@ -151,5 +151,5 @@ def predictive_modeling_group(df, x='participant_group', y='roc_auc_score', titl
     fig.update_traces(meanline_visible=True, box_visible=False)
     fig.update_layout(violingap=0, violinmode='overlay', title=title)
     fig.update_xaxes(showticklabels=False)
-    fig.update_yaxes(title_text='ROC AUC')
+    fig.update_yaxes(title_text='ROC AUC', range=[0.4, 1])
     fig.show()
