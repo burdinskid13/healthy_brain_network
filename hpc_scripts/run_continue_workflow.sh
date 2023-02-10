0680b421f8049f79a53a23620cabc10f07aa587e
Maedbh King <mking86@uwo.ca>
2023-02-10 11:40:55 -0800

    update continue workflow

--- hpc_scripts/run_continue_workflow.sh
+++ hpc_scripts/run_continue_workflow.sh
@@ -31,13 +31,13 @@ username=maedbh
 echo $("running continue workflow")
 
 # get timestamps
-base_dir=/om2/user/${username}/healthy_brain_network/model_specs
-TIMESTAMPS=$(ls -d1 "${base_dir}/"*2023*)
+cd /om2/user/${username}/healthy_brain_network/model_specs
+TIMESTAMPS=(*2023*/)
 
+cd /om2/user/${username}/healthy_brain_network/hbn/scripts
 for ((m=0; m<${#TIMESTAMPS[@]}; m++)); do \
 
 # loop over workflows and check which ones need to be continued
-cd /om2/user/${username}/healthy_brain_network/hbn/scripts
 python3 run_phenotype_models.py --spec_dir=${TIMESTAMPS[m]} --cachedir=/om2/user/${username}/bin/.cache/pydra-ml/cache-wf/; done
 
 # delete pydra-ml cache from openmind (takes up to omuch space)
