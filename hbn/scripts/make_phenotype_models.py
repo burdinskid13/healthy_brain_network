69d7c9b50a74946ea7d2769777637d1cc2d6b4e9
Maedbh King <mking86@uwo.ca>
2023-02-14 19:06:55 -0500

    fixed bug in make phenotype models

--- hbn/scripts/make_phenotype_models.py
+++ hbn/scripts/make_phenotype_models.py
@@ -50,19 +50,14 @@ def run(
     if out_dir is None:
         out_dir = Defaults.MODEL_SPEC_DIR
 
-    try: 
-        for feature in features:
-            model_spec, model_features = predictive_modeling.make_model(
-                                        feature_spec=os.path.join(Defaults.FEATURE_DIR, feature),
-                                        target_spec=os.path.join(Defaults.FEATURE_DIR, target),
-                                        pydraml_spec=os.path.join(Defaults.MODEL_SPEC_DIR, pydraml_spec),
-                                        participant_spec=os.path.join(Defaults.MODEL_SPEC_DIR, participant_spec),
-                                        out_dir=out_dir
-                                        )
-        return model_spec, model_features
-    except:
-        print(f'cannot create {feature}')
-    
+    for feature in features:
+        model_spec, model_features = predictive_modeling.make_model(
+                                    feature_spec=os.path.join(Defaults.FEATURE_DIR, feature),
+                                    target_spec=os.path.join(Defaults.FEATURE_DIR, target),
+                                    pydraml_spec=os.path.join(Defaults.MODEL_SPEC_DIR, pydraml_spec),
+                                    participant_spec=os.path.join(Defaults.MODEL_SPEC_DIR, participant_spec),
+                                    out_dir=out_dir
+                                    )
 
 if __name__ == "__main__":
     run()
