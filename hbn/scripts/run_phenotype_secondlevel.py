c54f063ab2c45d6be7239955a1f9e852f26bbbee
Maedbh King <mking86@uwo.ca>
2023-03-08 09:32:48 -0500

    modify secondlevel

--- hbn/scripts/run_phenotype_secondlevel.py
+++ hbn/scripts/run_phenotype_secondlevel.py
@@ -2,7 +2,7 @@ import warnings
 from hbn.constants import Defaults
 warnings.filterwarnings("ignore")
 
-def run(filter='2023-03-02'):
+def run(filter='2023-03-07'):
     """run second level modeling pipeline
     """
     import glob
