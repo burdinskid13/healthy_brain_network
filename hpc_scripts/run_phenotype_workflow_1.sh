bb30c675cef57720da56c803132f34c1d0402474
Maedbh King <mking86@uwo.ca>
2023-04-01 18:16:38 -0400

    update hpc scripts - increase memory

--- hpc_scripts/run_phenotype_workflow_1.sh
+++ hpc_scripts/run_phenotype_workflow_1.sh
@@ -12,10 +12,10 @@
 #SBATCH -c 16 # 16 hyperthreaded cores 
 #
 # Memory:
-#SBATCH --mem=4G
+#SBATCH --mem=7G
 #
 # Wall clock limit:
-#SBATCH --time=15:00:00 
+#SBATCH --time=20:00:00 
 # 
 # Email Updates:
 #SBATCH --mail-user=maedbh@mit.edu
