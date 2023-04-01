d9dc244eb426d0d7ee428e4f5d9a2228daa26f01
Maedbh King <mking86@uwo.ca>
2023-04-01 12:52:25 -0400

    update hpc scripts - all feature models

--- hpc_scripts/run_phenotype_workflow_1.sh
+++ hpc_scripts/run_phenotype_workflow_1.sh
@@ -12,10 +12,10 @@
 #SBATCH -c 16 # 16 hyperthreaded cores 
 #
 # Memory:
-#SBATCH --mem=3G
+#SBATCH --mem=4G
 #
 # Wall clock limit:
-#SBATCH --time=04:00:00 
+#SBATCH --time=15:00:00 
 # 
 # Email Updates:
 #SBATCH --mail-user=maedbh@mit.edu
