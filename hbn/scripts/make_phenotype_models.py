7d78f83d030f4765d1b15478a89ae3c68d790ccc
Maedbh King <mking86@uwo.ca>
2023-04-07 18:19:27 -0400

    modify participant_spec arg to make_phenotype_models

--- hbn/scripts/make_phenotype_models.py
+++ hbn/scripts/make_phenotype_models.py
@@ -15,10 +15,10 @@ class PythonLiteralOption(click.Option):
 
 @click.command()
 @click.option("--features", required=True, cls=PythonLiteralOption, default=[])
+@click.option("--participant_specs", required=True, cls=PythonLiteralOption, default=[])
 @click.option("--target", required=False)
 @click.option("--pydraml_spec", required=False)
 @click.option("--out_dir", required=False)
-@click.option('--participant_spec', required=False)
 
 
 def run(
@@ -26,7 +26,7 @@ def run(
     target='target_DX_01_Cat_new_binarize-spec.json',
     pydraml_spec='pydraml_spec2.json',
     out_dir=Defaults.MODEL_SPEC_DIR,
-    participant_spec='participant_spec1.json'
+    participant_specs=[]
     ):
     """Make phenotype model(s) using the following:`feature specs`, `targets`, `participant_spec`, `pydraml_spec`  
     
@@ -35,7 +35,7 @@ def run(
         target (str): target spec filename. should be stored in 'Defaults.FEATURE_DIR'
         pydraml_spec (str): pydraml spec filename. should be stored in `out_dir`
         out_dir (str): where model spec files will be saved. Default is Defaults.MODEL_SPEC_DIR
-        participant_spec (str): participant spec filename. should be stored in `out_dir`
+        participant_spec (list of str): participant spec filename. should be stored in `out_dir`
     """
     from pathlib import Path
     import glob
@@ -53,15 +53,16 @@ def run(
         out_dir = Defaults.MODEL_SPEC_DIR
 
     for feature in features:
-        if Path(feature).name!='features-parent_spec.json':
-            predictive_modeling.make_model(
-                                feature_spec=os.path.join(Defaults.FEATURE_DIR, feature),
-                                target_spec=os.path.join(Defaults.FEATURE_DIR, target),
-                                pydraml_spec=os.path.join(Defaults.MODEL_SPEC_DIR, pydraml_spec),
-                                participant_spec=os.path.join(Defaults.MODEL_SPEC_DIR, participant_spec),
-                                drop_identifiers=True,
-                                out_dir=out_dir
-                                )
+        for participant in participant_specs:
+            if Path(feature).name!='features-parent_spec.json':
+                predictive_modeling.make_model(
+                                    feature_spec=os.path.join(Defaults.FEATURE_DIR, feature),
+                                    target_spec=os.path.join(Defaults.FEATURE_DIR, target),
+                                    pydraml_spec=os.path.join(Defaults.MODEL_SPEC_DIR, pydraml_spec),
+                                    participant_spec=os.path.join(Defaults.MODEL_SPEC_DIR, participant),
+                                    drop_identifiers=True,
+                                    out_dir=out_dir
+                                    )
 
 if __name__ == "__main__":
     run()
