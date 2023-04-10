687d1df34f7c3641960efdaf0b2226e1e067093a
Maedbh King <mking86@uwo.ca>
2023-04-10 15:51:26 -0400

    increase time for item analysis

--- hbn/models/predictive_modeling.py
+++ hbn/models/predictive_modeling.py
@@ -177,33 +177,33 @@ def evaluation(results_dir, test_spec):
             feature_names = res[1].output.feature_names
             fitted_model = res[1].output.model
 
-        # get model spec info for fitted model
-        fpath = glob.glob(os.path.join(Path(results_dir).parent, '*-spec.json*'))[0]
-        feature_spec = io.read_json(fpath)['feature_spec']
-        target_spec = io.read_json(fpath)['target_spec']
-        test_spec = io.read_json(os.path.join(Defaults.MODEL_SPEC_DIR, test_spec))
+            # get model spec info for fitted model
+            fpath = glob.glob(os.path.join(Path(results_dir).parent, '*-spec.json*'))[0]
+            feature_spec = io.read_json(fpath)['feature_spec']
+            target_spec = io.read_json(fpath)['target_spec']
+            test_spec = io.read_json(os.path.join(Defaults.MODEL_SPEC_DIR, test_spec))
 
-        # get test data
-        X, y = get_test(feature_spec=feature_spec, target_spec=target_spec, test_spec=test_spec)
+            # get test data
+            X, y = get_test(feature_spec=feature_spec, target_spec=target_spec, test_spec=test_spec, feature_names=feature_names)
 
-        # get predictions 
-        y_pred = fitted_model.predict(X)
+            # get predictions 
+            y_pred = fitted_model.predict(X)
 
-        # get rmse
-        rmse = mean_squared_error(y, y_pred, squared=False)
-        R = calculate_R(y, y_pred)
-        R2 = calculate_R2(y, y_pred)
+            # get rmse
+            rmse = mean_squared_error(y, y_pred, squared=False)
+            R = calculate_R(y, y_pred)
+            R2 = calculate_R2(y, y_pred)
 
-        # make dataframe
-        df['y'] = y
-        df['y_pred'] = y_pred
-        df['train'] = io.read_json(fpath)['participant_spec']['diagnoses'][0]
-        df['predict'] = test_spec['diagnoses'][0]
-        df['split'] = test_spec['split']
+            # make dataframe
+            df['y'] = y
+            df['y_pred'] = y_pred
+            df['train'] = io.read_json(fpath)['participant_spec']['diagnoses'][0]
+            df['predict'] = test_spec['diagnoses'][0]
+            df['split'] = test_spec['split']
 
     return df
 
-def get_test(feature_spec, target_spec, test_spec):
+def get_test(feature_spec, target_spec, test_spec, feature_names):
     import os
     import pandas as pd
     from hbn import io
